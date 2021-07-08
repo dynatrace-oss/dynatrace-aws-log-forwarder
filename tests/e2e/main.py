70b4fac5682894dfca35a1ae6fbdc64e5115bffa
Kamil Sokołowski <kamil.sokolowski@dynatrace.com>
2021-07-08 14:46:04 +0200

    Generate test log event

--- tests/e2e/main.py
+++ tests/e2e/main.py
@@ -1,35 +1,49 @@
 #!/usr/bin/env python3
 
 import argparse
+from botocore.exceptions import ClientError
 import boto3
+import time
+
+epoch_timestamp_in_ms = int(time.time() * 1000)
 
 # Create AWS CloudWatch client
 aws_client = boto3.client('logs')
 
-def create_log_group(log_group_name,
-                     log_group_tags={
-                         'app': 'dynatrace',
-                         'env': 'develop',
-                         'tier': 'monitoring',
-                         'cloudformation': 'false',
-                     }):
+def generate_log_event(log_group_name,log_stream_name):
     """
-    Create a AWS CloudWatch Logs log group
+    Upload a log event to the specified AWS CloudWatch Logs log group
     """
 
-    aws_client.create_log_group(
-        logGroupName = log_group_name,
-        tags =         log_group_tags
-    )
-
-def delete_log_group(log_group_name):
-    """
-    Delete a AWS CloudWatch Logs log group
-    """
-
-    aws_client.delete_log_group(
-        logGroupName = log_group_name
-    )
+    try:
+        log_event = aws_client.put_log_events(
+            logGroupName  = log_group_name,
+            logStreamName = log_stream_name,
+            logEvents=[
+                {
+                    'timestamp': epoch_timestamp_in_ms,
+                    'message': 'This is a test log for ' + str(log_group_name)
+                },
+            ],
+        )
+        print("Generated log event: %s" % log_event)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DataAlreadyAcceptedException' or e.response['Error']['Code'] == 'InvalidSequenceTokenException':
+            print("Given log batch has already been accepeted. Retrying with the following sequence token: " + e.response['expectedSequenceToken'])
+            log_event_retry = aws_client.put_log_events(
+                logGroupName  = log_group_name,
+                logStreamName = log_stream_name,
+                logEvents=[
+                    {
+                        'timestamp': epoch_timestamp_in_ms,
+                        'message': 'This is a test log for ' + str(log_group_name)
+                    },
+                ],
+                sequenceToken = e.response['expectedSequenceToken']
+            )
+            print("Generated log event: %s" % log_event_retry)
+        else:
+            raise(e)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -40,7 +54,14 @@ if __name__ == '__main__':
                         default='',
                         required=True,
                         help='specify AWS CloudWatch Logs log group name')
+    parser.add_argument('--log-stream-name',
+                        '-s',
+                        action='store',
+                        type=str,
+                        default='',
+                        required=True,
+                        help='specify AWS CloudWatch Logs log stream name for a given log group')
     args = parser.parse_args()
 
-    create_log_group(args.log_group_name)
-    delete_log_group(args.log_group_name)
+    # Generate a test log event
+    generate_log_event(args.log_group_name,args.log_stream_name)
