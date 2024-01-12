060ec26f0e9390d3fe604ebbc8035caa545f1a1c
Kozimov <nematullo.kozimov@dynatrace.com>
2024-01-12 15:57:15 +0100

    PCLOUDS-2277 Multi-account logs support

--- src/logs/models/batch_metadata.py
+++ src/logs/models/batch_metadata.py
@@ -14,6 +14,5 @@
 
 class BatchMetadata:
     def __init__(self, account_id, region, partition):
-        self.account_id = account_id
         self.region = region
         self.partition = partition
